95d7748712e03e457f41f0d6b0cb71b22df2ab30
Curzy <cms9597@gmail.com>
2017-02-26 23:57:05 +0900

    First step of functional tests for layout and style

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -16,6 +16,28 @@ class NewVisitorTest(LiveServerTestCase):
         rows = table.find_elements_by_tag_name('tr')
         self.assertIn(row_text, [row.text for row in rows])
 
+    def test_layout_and_stlyling(self):
+        # Edith는 메인 페이지를 방문한다
+        self.browser.get(self.live_server_url)
+        self.browser.set_window_size(1024, 768)
+
+        # 그녀는 입력 상자가 가운데 배치된 것을 본다
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
+        # 그녀는 새로운 리스트를 시작하고 입력 상자가 가운데 배치된 것을 확인한다
+        inputbox.send_keys('testing\n')
+        inputbox = self.browser.find_element_by_id('id_new_item')
+        self.assertAlmostEqual(
+            inputbox.location['x'] + inputbox.size['width'] / 2,
+            512,
+            delta=10
+        )
+
     def test_can_start_a_list_and_retrieve_it_later(self):
 
         # 새로운 사용자 Edith 웹 사이트 접속
