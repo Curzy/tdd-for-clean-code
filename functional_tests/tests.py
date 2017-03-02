7a2937a22fe7af28ec7fceb2ceb022f665f90ef2
Curzy <cms9597@gmail.com>
2017-03-02 18:37:28 +0900

    Hack on functional test for staging test

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -1,9 +1,24 @@
+import sys
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium import webdriver
 from selenium.webdriver.common.keys import Keys
 
 
 class NewVisitorTest(StaticLiveServerTestCase):
+    @classmethod
+    def setUpClass(cls):
+        for arg in sys.argv:
+            if 'liveserver' in arg:
+                cls.server_url = 'http://' + arg.split('=')[1]
+                return
+        super().setUpClass()
+        cls.server_url = cls.live_server_url
+
+    @classmethod
+    def tearDownClass(cls):
+        if cls.server_url == cls.live_server_url:
+            super().tearDownClass()
+
     def setUp(self):
         self.browser = webdriver.Chrome()
         self.browser.implicitly_wait(3)
@@ -18,7 +33,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
     def test_layout_and_stlyling(self):
         # Edith는 메인 페이지를 방문한다
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         self.browser.set_window_size(1024, 768)
 
         # 그녀는 입력 상자가 가운데 배치된 것을 본다
@@ -41,7 +56,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
     def test_can_start_a_list_and_retrieve_it_later(self):
 
         # 새로운 사용자 Edith 웹 사이트 접속
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
 
         # 웹 페이지 타이틀, 헤더 'To-Do'
         self.assertIn('To-Do', self.browser.title)
@@ -83,7 +98,7 @@ class NewVisitorTest(StaticLiveServerTestCase):
 
         # Francis가 홈페이지에 접속
         # Edith의 리스트는 보이지 않는다
-        self.browser.get(self.live_server_url)
+        self.browser.get(self.server_url)
         page_text = self.browser.find_element_by_tag_name('body').text
         self.assertNotIn('공작깃털 사기', page_text)
         self.assertNotIn('그물 만들기', page_text)
